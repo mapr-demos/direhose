66cedb713a60aaf882a8b5d6269ddd06545dfc66
Michael Hausenblas <michael.hausenblas@gmail.com>
2014-02-11 06:55:24 +0000

    init core dir walking and doc

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-direhose
-========
+# direhose
 
-Traverses POSIX compliant filesystems and generates a data stream out of the discovered file and directory information.
+This util traverses POSIX compliant filesystems and generates a UDP data stream 
+out of the discovered file and directory information. The name `direhose` is a 
+portmanteau word, combining *directory* and *firehose*.
+
+## Dependencies
+
+* Python 2.6+
+* POSIX compliant file system
+
+## Usage
+
+Download/clone the repo and place it somewhere, such as `~/direhose`. Then you
+can run it as follows:
+
+    $ python direhose.py
+
+If you don't provide an input, `direhose` will traverse the filesystem starting
+from the current directory in depth-first order.
+
+## License
+
+[Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0.html).
--- direhose.py
+++ direhose.py
@@ -15,6 +15,7 @@ import logging
 import string
 import datetime
 import json
+import getopt
 
 ################################################################################
 ## config
@@ -22,8 +23,8 @@ import json
 DEBUG = False
 
 # defines host and port to stream the data out via UDP
-STREAM_HOST = 127.0.0.1
-STREAM_PORT = 6900
+STREAM_HOST = '127.0.0.1'
+STREAM_PORT = 7654
 
 # defines the interval in sec where progress will be reported to stdout
 REPORTING_INTERVAL = 10 
@@ -53,34 +54,37 @@ def _fs_create_package():
 def _send_package(out_socket, package):
     out_socket.sendto(str(package) + '\n', (STREAM_HOST, STREAM_PORT))
 
-def walk():
-  start_time = datetime.datetime.now()
-  num_packages = 0
-  bytes_packages = 0
+def walk(start_dir):
+  for root, dirs, files in os.walk(start_dir):
+    print('%s' %root)
+    for f in files:
+      print(' %s' %f)
+      # (package, package_size) = _fs_create_package()
+      # _send_fintran(out_socket, json.dumps(package))
 
-  while True:    
-    (package, package_size) = _fs_create_package()
-    _send_fintran(out_socket, json.dumps(package))
-    num_packages += 1
-    bytes_packages += package_size
-    end_time = datetime.datetime.now()
-    diff_time = end_time - start_time
+def usage():
+  print('Usage: python direhose.py [start directory]\n')
 
-    if diff_time.seconds > REPORTING_INTERVAL):
-      tp_bytes = bytes_packages/1024/1024
-      logging.info('%s\t%d\t%0.3f'
-        %(
-          str(datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')),
-          num_packages, 
-          bytes_packages/1024/1024
-        )
-      )
-      start_time = datetime.datetime.now()
-      num_packages = 0
-      bytes_packages = 0
 
 ################################################################################
 ## main script
 
 if __name__ == '__main__':
-  walk()
+  start_dir = '.'  
+  try:
+    opts, args = getopt.getopt(sys.argv[1:], 'h', ['help'])
+    
+    for opt, arg in opts:
+      if opt in ('-h', '--help'):
+        usage()
+        sys.exit()
+    try:
+      start_dir = args[0]
+    except:
+      pass
+  
+    walk(start_dir)
+  except getopt.GetoptError, err:
+    print str(err)
+    usage()
+    sys.exit(2)
